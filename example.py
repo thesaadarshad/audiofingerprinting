60996ee333f078bf511ab88777fd118d146fb061
thesaadarshad <thesaadarshad@gmail.com>
2016-12-13 02:53:05 +0500

    taking it locally

--- example.py
+++ example.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
 	djv = Dejavu(config)
 
 	# Fingerprint all the mp3's in the directory we give it
-	djv.fingerprint_directory("mp3", [".mp3"])
+	#djv.fingerprint_directory("mp3", [".mp3"])
 
 	# Recognize audio from a file
-	song = djv.recognize(FileRecognizer, "mp3/Sean-Fournier--Falling-For-You.mp3")
-	print "From file we recognized: %s\n" % song
+	#song = djv.recognize(FileRecognizer, "mp3/The-Lights-Galaxia--While-She-Sleeps.mp3")
+	#print "From file we recognized: %s\n" % song
 
 	# Or recognize audio from your microphone for `secs` seconds
 	secs = 5
@@ -30,6 +30,6 @@ if __name__ == '__main__':
 		print "From mic with %d seconds we recognized: %s\n" % (secs, song)
 
 	# Or use a recognizer without the shortcut, in anyway you would like
-	recognizer = FileRecognizer(djv)
-	song = recognizer.recognize_file("mp3/Josh-Woodward--I-Want-To-Destroy-Something-Beautiful.mp3")
-	print "No shortcut, we recognized: %s\n" % song
+	#recognizer = FileRecognizer(djv)
+	#song = recognizer.recognize_file("mp3/Choc--Eigenvalue-Subspace-Decomposition.mp3")
+	#print "No shortcut, we recognized: %s\n" % song
